1ccb8189f9dab77432b837ddf6c7a361c4c5b296
Natasha K <kapr.natasha@gmail.com>
2018-06-30 20:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello, everybody!";
+var greeting = "";
 
-var specialGuest = "Hello, Neil deGrasse Tyson!"
+var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "Let's talk about space";
+var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}.`;
+var conversation = `${topic}`;
